a84c6226bf7d09edb329d0d98f1d45d0bbcac367
yingdahu <yingdahu2018@u.northwestern.edu>
2018-05-11 20:06:31 +0000

    adding initial twilio communication

--- FaceDetectFaster.py
+++ FaceDetectFaster.py
@@ -10,13 +10,28 @@ import time
 import cv2
 import sys
 import RPi.GPIO as GPIO
+from twilio.rest import Client
 
 LEDPIN = 17
+TWILIO_ACCOUNT_SID = 'AC75fd9d7e943b776c2c26bc3acb524aee'
+TWILIO_AUTH_TOKEN = '06c1c2b77aa4f5854ea6e04b9e7670f3'
+TWILIO_NUMBER = '+16305213064'
+HOMEOWNER_NUMBER = '4082503360'
+
+def send_sms(to_number, body):
+    account_sid = TWILIO_ACCOUNT_SID
+    auth_token = TWILIO_AUTH_TOKEN
+    twilio_number = TWILIO_NUMBER
+    client = Client(account_sid, auth_token)
+    client.api.messages.create(to_number,
+                           from_=twilio_number,
+                           body=body)
 
 def sendTwilioMessage(message):
     print('sending twilio message:')
-    print(message)
+    send_sms(HOMEOWNER_NUMBER, message)
 
+    #if approved
     return True
 
 def unlockDoor():
@@ -67,6 +82,7 @@ def main():
             if faceDetected:
                 if(sendTwilioMessage('Here is a picture of your visitor, would you like to grant access?')):
                     unlockDoor()
+                    break
                     currentlyMonitoring = False
                     visitorEntered = False
                 else:
