fa46c6579a79d30f916b3a0cb5aeb6aed5cbd074
yingdahu <yingdahu2018@u.northwestern.edu>
2018-05-27 14:59:42 -0500

    Rename sendText.py to pinPad.py

---
+++ pinPad.py
@@ -0,0 +1,42 @@
+from matrix_keypad import RPi_GPIO
+import time
+import RPi.GPIO as GPIO
+
+DOORPIN = 2
+
+kp = RPi_GPIO.keypad()
+
+def unlockDoor():
+    print('unlocking door...')
+    GPIO.output(DOORPIN,True)
+    time.sleep(3)
+    print('locking door...')
+    GPIO.output(DOORPIN,False)
+
+def getDigit():
+    digitPressed = None
+    while digitPressed == None:
+        digitPressed = kp.getKey()
+    return digitPressed
+
+password = "3145"
+digits = ""
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(DOORPIN,GPIO.OUT)
+
+while(1):
+    digitPressed = getDigit()
+    if(digitPressed == '*'):
+        digits = ""
+    elif(digitPressed == "#"):
+        if(digits == password):
+            print "Password valid!"
+            unlockDoor()
+        else:
+            print "Invalid password!"    
+        digits = ""
+    else:
+        digits = digits + str(digitPressed)
+    print digits
+    time.sleep(0.5)
